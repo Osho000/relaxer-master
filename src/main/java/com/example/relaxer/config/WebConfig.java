92adebf0d28b6b515d00f0461c88df4cf463e04d
michoooooooooo <michoooooooooo@github.com>
2025-06-25 19:19:52 +0300

    WebConfig add

--- src/main/java/com/example/relaxer/config/WebConfig.java
+++ src/main/java/com/example/relaxer/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://10.0.0.158:8080")
+                        .allowedOrigins("http://localhost:3000")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
